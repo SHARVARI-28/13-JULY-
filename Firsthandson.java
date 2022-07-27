3025f9c6fa923c018573231b337203ed4f923704
SHARVARI-28 <78317768+SHARVARI-28@users.noreply.github.com>
2022-07-27 14:28:08 +0530

    Add files via upload

---
+++ Firsthandson.java
@@ -0,0 +1,22 @@
+ class Firsthandson {
+
+ public static void main(String[] args) {
+
+  int i=28;
+  if(i>0)
+  {
+	  System.out.println("I is Positive");
+  }
+  else if(i<0)
+  {
+	  System.out.println("I is negative");
+  }
+
+  else 
+  {
+	  System.out.println("I is Zero");
+  }
+
+ }
+
+}
